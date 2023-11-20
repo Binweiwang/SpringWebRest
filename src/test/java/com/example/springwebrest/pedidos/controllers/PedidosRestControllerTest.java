05afc84b9a91b9c3705011eb320da39bffb1b86a
Binweiwang <binweiwang1029@gmail.com>
2023-11-20 19:48:01 +0100

    test(pedidos): Agregados test de pedidos

---
+++ src/test/java/com/example/springwebrest/pedidos/controllers/PedidosRestControllerTest.java
@@ -0,0 +1,355 @@
+package com.example.springwebrest.pedidos.controllers;
+
+import com.example.springwebrest.rest.pedidos.exceptions.*;
+import com.example.springwebrest.rest.pedidos.models.Cliente;
+import com.example.springwebrest.rest.pedidos.models.Direccion;
+import com.example.springwebrest.rest.pedidos.models.LineaPedido;
+import com.example.springwebrest.rest.pedidos.models.Pedido;
+import com.example.springwebrest.rest.pedidos.services.PedidosService;
+import com.example.springwebrest.utils.pagination.PageResponse;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class) // Extensión de Mockito para usarlo
+class PedidosRestControllerTest {
+    private final String myEndpoint = "/pedidos";
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final Pedido pedido1 = Pedido.builder()
+            .id(new ObjectId("5f9f1a3b9d6b6d2e3c1d6f1a"))
+            .idUsuario(1L)
+            .cliente(
+                    new Cliente("Binwei", "binwei@binwei.com", "1234567890",
+                            new Direccion("Calle", "1", "Ciudad", "Provincia", "Pais", "12345")
+                    )
+            )
+            .lineasPedido(List.of(LineaPedido.builder()
+                    .idProducto(1L)
+                    .cantidad(2)
+                    .precioProducto(10.0)
+                    .build()))
+            .build();
+    @Autowired
+    MockMvc mockMvc; // Cliente MVC
+    @MockBean
+    private PedidosService pedidosService;
+
+    @Autowired
+    public PedidosRestControllerTest(PedidosService pedidosService) {
+        this.pedidosService = pedidosService;
+        mapper.registerModule(new JavaTimeModule()); // Necesario para que funcione LocalDateTime
+    }
+
+    @Test
+    void getAllPedidos() throws Exception {
+        var pedidosList = List.of(pedido1);
+        var pageable = PageRequest.of(0, 10, Sort.by("id").ascending());
+        var page = new PageImpl<>(pedidosList);
+
+        // Arrange
+        when(pedidosService.findAll(pageable)).thenReturn(page);
+
+        // Consulto el endpoint
+        MockHttpServletResponse response = mockMvc.perform(
+                        get(myEndpoint)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        PageResponse<Pedido> res = mapper.readValue(response.getContentAsString(), new TypeReference<>() {
+        });
+
+        // Assert
+        assertAll("findall",
+                () -> assertEquals(200, response.getStatus()),
+                () -> assertEquals(1, res.content().size())
+        );
+    }
+
+    @Test
+    void getPedidoById() throws Exception {
+        // Arrange
+        var myLocalEndpoint = myEndpoint + "/5f9f1a3b9d6b6d2e3c1d6f1a";
+        when(pedidosService.findById(any(ObjectId.class))).thenReturn(pedido1);
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        get(myLocalEndpoint)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        Pedido res = mapper.readValue(response.getContentAsString(), Pedido.class);
+
+        // Assert
+        assertAll(
+                () -> assertEquals(200, response.getStatus()),
+                () -> assertEquals(pedido1, res)
+        );
+
+    }
+
+    @Test
+    void getPedidoByIdNoFound() throws Exception {
+        // Arrange
+        var myLocalEndpoint = myEndpoint + "/5f9f1a3b9d6b6d2e3c1d6f1a";
+
+        // Arrange
+        when(pedidosService.findById(any(ObjectId.class)))
+                .thenThrow(new PedidoNotFound("5f9f1a3b9d6b6d2e3c1d6f1a"));
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        get(myLocalEndpoint)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // Assert
+        assertAll(
+                () -> assertEquals(404, response.getStatus())
+        );
+
+    }
+
+    @Test
+    void getPedidosByUsuario() throws Exception {
+        // Arrange
+        var myLocalEndpoint = myEndpoint + "/usuario/1";
+        var pedidosList = List.of(pedido1);
+        var pageable = PageRequest.of(0, 10, Sort.by("id").ascending());
+        var page = new PageImpl<>(pedidosList);
+
+        // Arrange
+        when(pedidosService.findByIdUsuario(anyLong(), any(Pageable.class))).thenReturn(page);
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        get(myLocalEndpoint)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        PageResponse<Pedido> res = mapper.readValue(response.getContentAsString(), new TypeReference<>() {
+        });
+
+        // Assert
+        assertAll(
+                () -> assertEquals(200, response.getStatus()),
+                () -> assertEquals(1, res.content().size())
+        );
+
+    }
+
+    @Test
+    void createPedido() throws Exception {
+        // Arrange
+        when(pedidosService.save(any(Pedido.class))).thenReturn(pedido1);
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        post(myEndpoint)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                // Le paso el body
+                                .content(mapper.writeValueAsString(pedido1)))
+                .andReturn().getResponse();
+
+        Pedido res = mapper.readValue(response.getContentAsString(), Pedido.class);
+
+        // Assert
+        assertAll(
+                () -> assertEquals(201, response.getStatus()),
+                () -> assertEquals(pedido1, res)
+        );
+    }
+
+    @Test
+    void createPedidoNoItemsBadRequest() throws Exception {
+        // Arrange
+        when(pedidosService.save(any(Pedido.class))).thenThrow(new PedidoNotItems("5f9f1a3b9d6b6d2e3c1d6f1a"));
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        post(myEndpoint)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                // Le paso el body
+                                .content(mapper.writeValueAsString(pedido1)))
+                .andReturn().getResponse();
+
+        // Assert
+        assertAll(
+                () -> assertEquals(400, response.getStatus())
+        );
+    }
+
+    @Test
+    void createPedidoBadPrice() throws Exception {
+        // Arrange
+        when(pedidosService.save(any(Pedido.class))).thenThrow(new ProductoBadPrice(1L));
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        post(myEndpoint)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                // Le paso el body
+                                .content(mapper.writeValueAsString(pedido1)))
+                .andReturn().getResponse();
+
+        // Assert
+        assertAll(
+                () -> assertEquals(400, response.getStatus())
+        );
+    }
+
+    @Test
+    void getPedidosNotFoundBadRequest() throws Exception {
+        // Arrange
+        when(pedidosService.save(any(Pedido.class))).thenThrow(new ProductoNotFound(1L));
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        post(myEndpoint)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                // Le paso el body
+                                .content(mapper.writeValueAsString(pedido1)))
+                .andReturn().getResponse();
+
+        // Assert
+        assertAll(
+                () -> assertEquals(400, response.getStatus())
+        );
+    }
+
+    @Test
+    void getPedidosNotStockBadRequest() throws Exception {
+        // Arrange
+        when(pedidosService.save(any(Pedido.class))).thenThrow(new ProductoNotStock(1L));
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        post(myEndpoint)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                // Le paso el body
+                                .content(mapper.writeValueAsString(pedido1)))
+                .andReturn().getResponse();
+
+        // Assert
+        assertAll(
+                () -> assertEquals(400, response.getStatus())
+        );
+    }
+
+    @Test
+    void update() throws Exception {
+        var myLocalEndpoint = myEndpoint + "/5f9f1a3b9d6b6d2e3c1d6f1a";
+
+        // Arrange
+        when(pedidosService.update(any(ObjectId.class), any(Pedido.class))).thenReturn(pedido1);
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        put(myLocalEndpoint)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(pedido1)))
+                .andReturn().getResponse();
+
+        Pedido res = mapper.readValue(response.getContentAsString(), Pedido.class);
+
+        // Assert
+        assertAll(
+                () -> assertEquals(200, response.getStatus()),
+                () -> assertEquals(pedido1, res)
+        );
+    }
+
+    @Test
+    void updatePedidoNoFound() throws Exception {
+        var myLocalEndpoint = myEndpoint + "/5f9f1a3b9d6b6d2e3c1d6f1a";
+
+        // Arrange
+        when(pedidosService.update(any(ObjectId.class), any(Pedido.class)))
+                .thenThrow(new PedidoNotFound("5f9f1a3b9d6b6d2e3c1d6f1a"));
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        put(myLocalEndpoint)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                // Le paso el body
+                                .content(mapper.writeValueAsString(pedido1)))
+                .andReturn().getResponse();
+
+        // Assert
+        assertAll(
+                () -> assertEquals(404, response.getStatus())
+        );
+    }
+
+
+    @Test
+    void deletePedido() throws Exception {
+        var myLocalEndpoint = myEndpoint + "/5f9f1a3b9d6b6d2e3c1d6f1a";
+
+        // Arrange
+        doNothing().when(pedidosService).delete(any(ObjectId.class));
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        delete(myLocalEndpoint)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // Assert
+        assertAll(
+                () -> assertEquals(204, response.getStatus())
+        );
+
+
+    }
+
+    @Test
+    void deletePedidoNoFound() throws Exception {
+        var myLocalEndpoint = myEndpoint + "/5f9f1a3b9d6b6d2e3c1d6f1a";
+
+        // Arrange
+        doThrow(new PedidoNotFound("5f9f1a3b9d6b6d2e3c1d6f1a")).when(pedidosService).delete(any(ObjectId.class));
+
+        // Act
+        MockHttpServletResponse response = mockMvc.perform(
+                        delete(myLocalEndpoint)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // Assert
+        assertAll(
+                () -> assertEquals(404, response.getStatus())
+        );
+    }
+}
---
+++ src/test/java/com/example/springwebrest/pedidos/services/PedidosServiceImplTest.java
@@ -0,0 +1,218 @@
+package com.example.springwebrest.pedidos.services;
+
+import com.example.springwebrest.rest.funko.models.Funko;
+import com.example.springwebrest.rest.funko.repository.FunkoRepository;
+import com.example.springwebrest.rest.pedidos.exceptions.PedidoNotFound;
+import com.example.springwebrest.rest.pedidos.exceptions.PedidoNotItems;
+import com.example.springwebrest.rest.pedidos.models.LineaPedido;
+import com.example.springwebrest.rest.pedidos.models.Pedido;
+import com.example.springwebrest.rest.pedidos.repositories.PedidosRepository;
+import com.example.springwebrest.rest.pedidos.services.PedidosServiceImpl;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PedidosServiceImplTest {
+    @Mock
+    private PedidosRepository pedidosRepository;
+    @Mock
+    private FunkoRepository funkoRepository;
+
+    @InjectMocks
+    private PedidosServiceImpl pedidosService;
+
+    @Test
+    void findAll() {
+        // Arrange
+        List<Pedido> pedidos = List.of(new Pedido(), new Pedido());
+        Page<Pedido> expectedPage = new PageImpl<>(pedidos);
+        Pageable pageable = PageRequest.of(0, 10);
+
+        when(pedidosRepository.findAll(pageable)).thenReturn(expectedPage);
+
+        // Act
+        Page<Pedido> result = pedidosService.findAll(pageable);
+
+        // Assert
+        assertAll(
+                () -> assertEquals(expectedPage, result),
+                () -> assertEquals(expectedPage.getContent(), result.getContent()),
+                () -> assertEquals(expectedPage.getTotalElements(), result.getTotalElements())
+        );
+    }
+
+    @Test
+    void findById() {
+        // Arrange
+        ObjectId idPedido = new ObjectId();
+        Pedido expectedPedido = new Pedido();
+        when(pedidosRepository.findById(idPedido)).thenReturn(Optional.of(expectedPedido));
+
+        // Act
+        Pedido resultPedido = pedidosService.findById(idPedido);
+
+        // Assert
+        assertEquals(expectedPedido, resultPedido);
+
+    }
+
+    @Test
+    void findByIdNotFound() {
+        // Arrange
+        ObjectId idPedido = new ObjectId();
+        when(pedidosRepository.findById(idPedido)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(PedidoNotFound.class, () -> pedidosService.findById(idPedido));
+
+        // Verify
+        verify(pedidosRepository).findById(idPedido);
+    }
+
+    @Test
+    void findByIdUsuario() {
+        // Arrange
+        Long idUsuario = 1L;
+        Pageable pageable = mock(Pageable.class);
+        @SuppressWarnings("unchecked")
+        Page<Pedido> expectedPage = mock(Page.class);
+        when(pedidosRepository.findByIdUsuario(idUsuario, pageable)).thenReturn(expectedPage);
+
+        // Act
+        Page<Pedido> resultPage = pedidosService.findByIdUsuario(idUsuario, pageable);
+
+        // Assert
+        assertEquals(expectedPage, resultPage);
+
+        // Verify
+        verify(pedidosRepository).findByIdUsuario(idUsuario, pageable);
+    }
+
+    @Test
+    void save() {
+        // Arrange
+        Funko funko = Funko.builder()
+                .id(1L)
+                .quantity(5)
+                .price(10.0)
+                .build();
+
+        Pedido pedido = new Pedido();
+        LineaPedido lineaPedido = LineaPedido.builder()
+                .idProducto(1L)
+                .cantidad(2)
+                .precioProducto(10.0)
+                .build();
+        pedido.setLineasPedido(List.of(lineaPedido));
+        Pedido pedidoToSave = new Pedido();
+        pedidoToSave.setLineasPedido(List.of(lineaPedido));
+
+        when(pedidosRepository.save(any(Pedido.class))).thenReturn(pedidoToSave); // Utiliza any(Pedido.class) para cualquier instancia de Pedido
+        when(funkoRepository.findById(anyLong())).thenReturn(Optional.of(funko));
+
+        // Act
+        Pedido resultPedido = pedidosService.save(pedido);
+
+        // Assert
+        assertAll(
+                () -> assertEquals(pedidoToSave, resultPedido),
+                () -> assertEquals(pedidoToSave.getLineasPedido(), resultPedido.getLineasPedido()),
+                () -> assertEquals(pedidoToSave.getLineasPedido().size(), resultPedido.getLineasPedido().size())
+        );
+    }
+
+
+    @Test
+    void saveNotItems() {
+        // Arrange
+        Pedido pedido = new Pedido();
+
+        // Act & Assert
+        assertThrows(PedidoNotItems.class, () -> pedidosService.save(pedido));
+    }
+
+    @Test
+    void delete() {
+        // Arrange
+        ObjectId idPedido = new ObjectId();
+        Pedido pedidoToDelete = new Pedido();
+        when(pedidosRepository.findById(idPedido)).thenReturn(Optional.of(pedidoToDelete));
+
+        // Act
+        pedidosService.delete(idPedido);
+    }
+
+    @Test
+    void deleteNotFound() {
+        // Arrange
+        ObjectId idPedido = new ObjectId();
+        when(pedidosRepository.findById(idPedido)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(PedidoNotFound.class, () -> pedidosService.delete(idPedido));
+
+    }
+
+    @Test
+    void update() {
+        // Arrange
+        Funko funko = Funko.builder()
+                .id(1L)
+                .quantity(5)
+                .price(10.0)
+                .build();
+
+
+        LineaPedido lineaPedido = LineaPedido.builder()
+                .idProducto(1L)
+                .cantidad(2)
+                .precioProducto(10.0)
+                .build();
+
+        ObjectId idPedido = new ObjectId();
+        Pedido pedido = new Pedido();
+        pedido.setLineasPedido(List.of(lineaPedido));
+        Pedido pedidoToUpdate = new Pedido();
+        pedidoToUpdate.setLineasPedido(List.of(lineaPedido)); // Inicializar la lista de líneas de pedido
+
+        when(pedidosRepository.findById(idPedido)).thenReturn(Optional.of(pedidoToUpdate));
+        when(pedidosRepository.save(any(Pedido.class))).thenReturn(pedidoToUpdate);
+        when(funkoRepository.findById(anyLong())).thenReturn(Optional.of(funko));
+
+        // Act
+        Pedido resultPedido = pedidosService.update(idPedido, pedido);
+
+        // Assert
+        assertAll(
+                () -> assertEquals(pedidoToUpdate, resultPedido),
+                () -> assertEquals(pedidoToUpdate.getLineasPedido(), resultPedido.getLineasPedido()),
+                () -> assertEquals(pedidoToUpdate.getLineasPedido().size(), resultPedido.getLineasPedido().size())
+        );
+    }
+
+    @Test
+    void updatePedidoNotFound() {
+        // Arrange
+        ObjectId idPedido = new ObjectId();
+        Pedido pedido = new Pedido();
+        when(pedidosRepository.findById(idPedido)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(PedidoNotFound.class, () -> pedidosService.update(idPedido, pedido));
+
+    }
+}
