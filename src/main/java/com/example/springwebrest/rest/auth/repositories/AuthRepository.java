593b418e214b5fb96485a53df80f7e483212e011
Binweiwang <binweiwang1029@gmail.com>
2023-11-23 20:40:20 +0100

    feat(authrepository): Agregado AuthRepository

---
+++ src/main/java/com/example/springwebrest/rest/auth/repositories/AuthRepository.java
@@ -0,0 +1,11 @@
+package com.example.springwebrest.rest.auth.repositories;
+
+import com.example.springwebrest.rest.users.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface AuthRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
