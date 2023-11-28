e0c9287d351a9f31efbad0163512f56fde1c7a31
Binweiwang <binweiwang1029@gmail.com>
2023-11-28 20:23:45 +0100

    fix(usersrepository): Arreglado un error

--- src/main/java/com/example/springwebrest/rest/users/repositories/UsersRepository.java
+++ src/main/java/com/example/springwebrest/rest/users/repositories/UsersRepository.java
@@ -1,6 +1,7 @@
 package com.example.springwebrest.rest.users.repositories;
 
 import com.example.springwebrest.rest.users.models.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
@@ -19,6 +20,7 @@ public interface UsersRepository extends JpaRepository<User, Long>, JpaSpecifica
     List<User> findAllByUsernameContainingIgnoreCase(String username);
 
     @Modifying
+    @Transactional
     @Query("UPDATE User p SET p.isDeleted = true WHERE p.id = :id")
     void updateIsDeletedToTrueById(Long id);
 }
